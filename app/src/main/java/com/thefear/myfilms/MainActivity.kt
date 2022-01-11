2cdda6db071038965cfea973d865eea3011020c6
TheFear36 <gpodvorotov@gmail.com>
2022-01-11 19:02:38 +0300

    Initial commit

---
+++ app/src/main/java/com/thefear/myfilms/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thefear.myfilms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
